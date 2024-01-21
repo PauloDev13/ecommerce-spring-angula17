41bbd4d98832a9912275453f6e4bdabe18874b3d
prmorais <prmorais1302@gmail.com>
2024-01-21 18:50:57 -0300

    ANG - Creating login and signup components and routes

--- e-commerce-front/src/app/app.component.ts
+++ e-commerce-front/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+const MATERIAL = [MatToolbarModule, MatButtonModule];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MATERIAL],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'e-commerce-front';
--- e-commerce-front/src/app/app.routes.ts
+++ e-commerce-front/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+];
