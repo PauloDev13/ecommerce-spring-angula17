0e8e8afe687391cc16cfade900010e75e177ccd4
prmorais <prmorais1302@gmail.com>
2024-01-29 10:24:31 -0300

    SB - Products search by name

--- e-commerce-front/src/app/admin/components/dashboard/dashboard.component.ts
+++ e-commerce-front/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,20 +1,64 @@
 import { NgForOf } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatCard } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
+import {
+  MatFormField,
+  MatLabel,
+  MatSuffix,
+} from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
+import { tap } from 'rxjs';
 
 import { AdminService } from '../../service/admin.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatCard, MatDivider, NgForOf, MatButton, RouterLink],
+  imports: [
+    MatCard,
+    MatDivider,
+    NgForOf,
+    MatButton,
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatSuffix,
+    MatIcon,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   private adminService = inject(AdminService);
-  protected readonly products = this.adminService.products;
+  protected products = this.adminService.listProducts;
+  protected products2 = this.adminService.listProducts2;
+  private formBuilder = inject(FormBuilder);
+  protected searchFormProduct = this.formBuilder.group({
+    title: [''],
+  });
+
+  onSubmit() {
+    const { title } = this.searchFormProduct.getRawValue();
+
+    if (title) {
+      this.adminService
+        .allProductsByName$(title)
+        .pipe(tap(res => this.products.update(() => [...res])))
+        .subscribe();
+
+      this.searchFormProduct.reset();
+    }
+  }
+
+  onReset() {
+    this.products.set(this.products2());
+    console.log(this.products2());
+  }
 }
--- e-commerce-front/src/app/admin/service/admin.service.ts
+++ e-commerce-front/src/app/admin/service/admin.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { tap } from 'rxjs';
+import { take, tap } from 'rxjs';
 
 import { CategoryRequestInterface } from '../../models/category-request.interface';
 import { CategoryResponseInterface } from '../../models/category-response.interface';
@@ -12,34 +12,41 @@ import { UserStorageService } from '../../services/user-storage.service';
   providedIn: 'root',
 })
 export class AdminService {
+  listProducts = signal<ProductResponseInterface[]>([]);
+  listProducts2 = signal<ProductResponseInterface[]>([]);
   private readonly BASE_URL: string = 'http://localhost:8080';
   private readonly http = inject(HttpClient);
-
-  // SEARCH PRODUCTS
-  private allProducts$ = this.http.get<ProductResponseInterface[]>(
-    `${this.BASE_URL}/api/admin/products`,
-    {
+  // SEARCH ALL PRODUCTS
+  private allProducts$ = this.http
+    .get<ProductResponseInterface[]>(`${this.BASE_URL}/api/admin/products`, {
       headers: this.authorizationHeader(),
-    },
-  );
+    })
+    .pipe(
+      take(1),
+      tap(res => {
+        this.listProducts.set(res);
+        this.listProducts2.set(res);
+      }),
+    )
+    .subscribe();
 
-  products = toSignal(this.allProducts$, {
-    initialValue: [] as ProductResponseInterface[],
-  });
-
-  // SEARCH CATEGORIES
+  // SIGNAL GET ALL PRODUCTS
+  // products = toSignal(this.allProducts$, {
+  //   initialValue: [] as ProductResponseInterface[],
+  // });
+  // SEARCH ALL CATEGORIES
   private allCategories$ = this.http.get<CategoryResponseInterface[]>(
     `${this.BASE_URL}/api/admin/categories`,
     {
       headers: this.authorizationHeader(),
     },
   );
-
+  // SIGNAL GET ALL CATEGORIES
   categories = toSignal(this.allCategories$, {
     initialValue: [] as CategoryResponseInterface[],
   });
 
-  // CREATE CATEGORY
+  // CREATE ONE CATEGORY
   createCategory$(category: CategoryRequestInterface) {
     return this.http.post<CategoryResponseInterface>(
       `${this.BASE_URL}/api/admin/category`,
@@ -48,7 +55,7 @@ export class AdminService {
     );
   }
 
-  // CREATE CATEGORY
+  // CREATE ONE PRODUCT
   createProduct$(product: FormData) {
     return this.http
       .post<ProductResponseInterface>(
@@ -60,10 +67,19 @@ export class AdminService {
       )
       .pipe(
         tap(productResp => {
-          this.products().push(productResp);
+          this.listProducts.update(res => [...res, productResp]);
         }),
       );
   }
+
+  // SEARCH ALL PRODUCTS BY NAME
+  allProductsByName$ = (name: string) =>
+    this.http.get<ProductResponseInterface[]>(
+      `${this.BASE_URL}/api/admin/search/${name}`,
+      {
+        headers: this.authorizationHeader(),
+      },
+    );
 
   // ADD TOKEN IN HEADER AUTHORIZATION
   private authorizationHeader(): HttpHeaders {
